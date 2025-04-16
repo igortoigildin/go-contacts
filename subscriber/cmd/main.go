41ddac2de19b38eb1add62c5af9ea5de8664ce4c
Igor Toigildin <toygildinigor@gmail.com>
2025-04-16 19:18:12 +0300

    add subscriber service

--- subscriber/cmd/main.go
+++ subscriber/cmd/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"os"
+	"time"
 
+	subscriber "github.com/igortoigildin/go-contacts/subscriber/internal/api"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	// Subscriber routes
+	e.POST("/subscriber/request", subscriber.MakeFriendRequestHandler)
+	e.POST("/subscriber/accept", subscriber.AcceptFriendHandler)
+	e.POST("/subscriber/reject", subscriber.RejectFriendHandler)
+	e.DELETE("/subscriber", subscriber.RemoveFriendHandler)
+	e.GET("/subscriber/:id", subscriber.GetFriendsHandler)
+
+	e.GET("/health/ready", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"status": "ok"})
+	})
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	time.Sleep(5 * time.Second)
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
 }
